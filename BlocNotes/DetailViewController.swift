db348b0c4421565f85326d5df72c0ffeb1515541
Adrian Bolinger <adrian.bolinger@me.com>
2015-02-20 12:08:03 -0500

    Share sheet added to DetailVC

--- BlocNotes/DetailViewController.swift
+++ BlocNotes/DetailViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    
+    
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
             self.configureView()
         }
     }
-
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail: AnyObject = self.detailItem {
@@ -28,18 +28,30 @@ class DetailViewController: UIViewController {
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - UIActivityViewController (aka "Share Sheet")
+    
+    @IBAction func shareNote(sender: AnyObject) {
+        if let noteToShare = detailDescriptionLabel.text {
+            
+            let objectsToShare = [noteToShare]
+            let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+            
+            //New Excluded Activities Code
+            activityVC.excludedActivityTypes = [UIActivityTypeAirDrop, UIActivityTypeAddToReadingList] // Leaving it in for now
+            self.presentViewController(activityVC, animated: true, completion: nil)
+        }
+    }
 }
 
